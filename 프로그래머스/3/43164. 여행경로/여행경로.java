25900b44899ded179bf08c4318d90e38099a80ef
esther <92513850+EunjinWoo@users.noreply.github.com>
2026-01-08 11:04:27 +0900

    [level 3] Title: 여행경로, Time: 0.67 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[][] tickets) {
+        
+        List<String> answer = new ArrayList<>();
+        boolean[] visited = new boolean[tickets.length];
+        
+        Arrays.sort(tickets, (a, b) -> {
+            if (a[0].equals(b[0])) {
+                return a[1].compareTo(b[1]);
+            }
+            return a[0].compareTo(b[0]);
+        });
+
+        answer.add("ICN");
+        dfs("ICN", answer, visited, tickets, 0);
+        
+        return answer.toArray(new String[0]);
+    }
+    
+    public boolean dfs(String start, List<String> answer, boolean[] visited, String[][] tickets, int usedCnt) {
+        if (usedCnt == tickets.length) {
+            return true;
+        }
+        
+        for (int i = 0; i < tickets.length; i++) {
+            if (!visited[i] && start.equals(tickets[i][0])) {
+                
+                visited[i] = true;
+                answer.add(tickets[i][1]);
+                
+                if (dfs(tickets[i][1], answer, visited, tickets, usedCnt + 1)) {
+                    return true;
+                }
+                
+                visited[i] = false;
+                answer.remove(answer.size() - 1);
+            }
+        }
+        
+        return false;
+    }
+}
